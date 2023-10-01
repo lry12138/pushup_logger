9c54dbf2ad41c89ae3a5412695b63ea05a7c60ef
Ruiyun Li <lry.ox.uk@gmail.com>
2023-10-01 16:10:55 +0100

    pushup logger basic structure

---
+++ pushup_logger/__init__.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def create_app():
+    app = Flask(__name__)
+
+    app.config['SECRET_KEY']= 'secret-key'
+    app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///db.sqlite'
+
+    db.init_app(app)
+
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint)
+    
+    import pushup_logger.models
+
+    with app.app_context():
+        db.create_all()
+
+    return app
---
+++ pushup_logger/auth.py
@@ -0,0 +1,32 @@
+from flask import Blueprint, render_template,url_for,request,redirect
+
+auth = Blueprint('auth',__name__)
+
+@auth.route('/register')
+def register():
+    return render_template('register.html')
+
+@auth.route('/register',methods = ['POST'])
+def register_post():
+    name = request.form.get('name')
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    print(email,name,password)
+    return redirect(url_for('auth.login'))
+
+@auth.route('/login')
+def login():
+    return render_template('login.html')
+
+@auth.route('/login',methods = ['POST'])
+def login_post():
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    print(email,password)
+    return redirect(url_for('main.profile'))
+
+@auth.route('/logout')
+def logout():
+    return " Logout page "
