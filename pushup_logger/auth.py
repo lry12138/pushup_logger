4b5aac975c28155e6b0a56a3c7e8cc40110050d9
Ruiyun Li <d>
2023-10-02 14:48:40 +0100

    login function added

--- pushup_logger/auth.py
+++ pushup_logger/auth.py
@@ -1,4 +1,8 @@
 from flask import Blueprint, render_template,url_for,request,redirect
+from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import login_user,logout_user,login_required,current_user
+from .models import User
+from . import db
 
 auth = Blueprint('auth',__name__)
 
@@ -12,7 +16,15 @@ def register_post():
     email = request.form.get('email')
     password = request.form.get('password')
 
-    print(email,name,password)
+    user = User.query.filter_by(email = email).first()
+    if user:
+        return redirect(url_for('auth.login'))
+    
+    else:
+        new_user = User(name = name,email = email,password = generate_password_hash(password,method='pbkdf2'))
+        db.session.add(new_user)
+        db.session.commit()
+    
     return redirect(url_for('auth.login'))
 
 @auth.route('/login')
@@ -23,10 +35,18 @@ def login():
 def login_post():
     email = request.form.get('email')
     password = request.form.get('password')
+    remember = True if request.form.get('remember') else False
 
-    print(email,password)
+    user = User.query.filter_by(email = email).first()
+    if not user or not check_password_hash(user.password,password):
+        return redirect(url_for('auth.login'))
+    
+    login_user(user,remember = remember)
+
     return redirect(url_for('main.profile'))
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return " Logout page "
+    logout_user()
+    return redirect(url_for('main.index'))
